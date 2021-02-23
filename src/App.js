8b12229884758759f940d218e4a96aa8392f7cb3
Paul Williams <paul.d.williamsxiv@gmail.com>
2021-02-22 20:34:36 -0500

    updated links in footer and created a page to display a single item in detail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Switch,Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/pages/DetailItem.js
+++ src/pages/DetailItem.js
@@ -1,5 +1,7 @@
 export default function DetailItem() {
     return (
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
